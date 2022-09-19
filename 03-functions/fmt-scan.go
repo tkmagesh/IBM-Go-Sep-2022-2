33098d045a783990aab80effbb5395909992958e
Magesh Kuppan <tkmagesh77@gmail.com>
2022-09-19 17:14:27 +0530

    Day-01 complete

---
+++ 03-functions/fmt-scan.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* var userName string
+	fmt.Println("Enter the name :")
+	fmt.Scanln(&userName)
+	fmt.Println(userName)
+
+	var firstName, lastName string
+	fmt.Println("Enter firstName & lastName")
+	fmt.Scanln(&firstName, &lastName)
+	fmt.Println("firstName : ", firstName, "lastName : ", lastName) */
+
+	var no int
+	fmt.Scanln(&no)
+	fmt.Println(no)
+}
