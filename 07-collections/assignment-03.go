317f77c128db3fb994abee8f157c971e1469f8a7
Magesh Kuppan <tkmagesh77@gmail.com>
2022-09-20 15:51:08 +0530

    assignments

---
+++ 07-collections/assignment-03.go
@@ -0,0 +1,7 @@
+/*
+Write a program that will print the prime numbers between start & end
+accept the start & end from the user
+
+refactor the program to encapsulate the prime number generation logic in a function accepting start and end as parameters
+the main function should be responsible for accpeting inputs and printing the result
+*/
