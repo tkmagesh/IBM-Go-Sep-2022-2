d54e6c7fd59475c597ae7e7599380902faeac03c
Magesh Kuppan <tkmagesh77@gmail.com>
2022-09-20 10:24:07 +0530

    Higher Order Functions - Pass functions as arguments to other functions

---
+++ 03-functions/05-demo.go
@@ -0,0 +1,55 @@
+/* Higher Order Functions - Pass functions as arguments to other functions */
+
+package main
+
+import "fmt"
+
+type operation func(int, int) int
+
+func main() {
+	exec(fn)
+
+	/*
+		logAdd(100, 200)
+		logSubtract(100, 200)
+	*/
+
+	logOperation(100, 200, add)
+	logOperation(100, 200, subtract)
+}
+
+func logOperation(x, y int, oper operation) {
+	fmt.Println("Invocation started")
+	fmt.Println(oper(x, y))
+	fmt.Println("Invocation completed")
+}
+
+/*
+func logAdd(x, y int) {
+	fmt.Println("Invocation started")
+	fmt.Println(add(x, y))
+	fmt.Println("Invocation completed")
+}
+
+func logSubtract(x, y int) {
+	fmt.Println("Invocation started")
+	fmt.Println(subtract(x, y))
+	fmt.Println("Invocation completed")
+}
+*/
+//3rd party library
+func add(x, y int) int {
+	return x + y
+}
+
+func subtract(x, y int) int {
+	return x - y
+}
+
+func exec(f func()) {
+	f()
+}
+
+func fn() {
+	fmt.Println("fn invoked")
+}
