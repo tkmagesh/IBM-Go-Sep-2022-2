30ca31b1b3455decc92e07b95f5bdf6e91b1e54a
Magesh Kuppan <tkmagesh77@gmail.com>
2022-09-20 14:53:37 +0530

    more on slices

--- 07-collections/02-slices.go
+++ 07-collections/02-slices.go
@@ -27,4 +27,34 @@ func main() {
 	for idx, val := range nos {
 		fmt.Printf("nos[%d] = %d\n", idx, val)
 	}
+
+	/*
+		newNos := nos
+		newNos[0] = 99
+		fmt.Println(nos, newNos)
+	*/
+
+	/*
+		slicing
+		[lo:hi] => from lo to hi-1
+		[lo:] => from lo to end of the slice
+		[:hi] => from 0 to hi-1
+	*/
+
+	fmt.Println("nos[3:6] =", nos[3:6])
+	fmt.Println("nos[3:] =", nos[3:])
+	fmt.Println("nos[:6] =", nos[:6])
+
+	subset := nos[3:6]
+	/*
+		subset[0] = 99
+		fmt.Println(subset, nos)
+	*/
+
+	//creating a copy of the slice
+	copyOfSubset := make([]int, len(subset), len(subset))
+	copy(copyOfSubset, subset)
+	copyOfSubset[0] = -100
+	fmt.Println(subset, copyOfSubset)
+
 }
