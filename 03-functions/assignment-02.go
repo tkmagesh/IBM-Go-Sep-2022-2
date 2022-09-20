3200e7bdfcef9f5a8b21e3bb78c3d2b69b1d515a
Magesh Kuppan <tkmagesh77@gmail.com>
2022-09-20 09:38:35 +0530

    assignment-02 solution

--- 03-functions/assignment-02.go
+++ 03-functions/assignment-02.go
@@ -20,3 +20,58 @@
 		print "Invalid choice"
 		display the menu again
 */
+package main
+
+import "fmt"
+
+func main() {
+
+	for {
+		userChoice := getUserChoice()
+		if userChoice == 5 {
+			break
+		}
+
+		if userChoice < 1 || userChoice > 5 {
+			fmt.Println("Invalid choice")
+			continue
+		}
+		n1, n2 := getOperands()
+		result := processUserChoice(userChoice, n1, n2)
+		fmt.Println("Result =", result)
+	}
+
+	fmt.Println("Thank you!")
+}
+
+func getUserChoice() (userChoice int) {
+	fmt.Println("1. Add")
+	fmt.Println("2. Subtract")
+	fmt.Println("3. Multiply")
+	fmt.Println("4. Divide")
+	fmt.Println("5. Exit")
+	fmt.Println("Enter the choice :")
+	fmt.Scanln(&userChoice)
+	return
+}
+
+func getOperands() (n1 int, n2 int) {
+	fmt.Println("Enter the nos:")
+	fmt.Scanln(&n1, &n2)
+	return
+}
+
+func processUserChoice(userChoice, n1, n2 int) int {
+	switch userChoice {
+	case 1:
+		return n1 + n2
+	case 2:
+		return n1 - n2
+	case 3:
+		return n1 * n2
+	case 4:
+		return n1 / n2
+	default:
+		return 0
+	}
+}
