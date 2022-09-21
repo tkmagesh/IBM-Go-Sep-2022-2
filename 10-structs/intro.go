85d83a76b53dc1492c7ee35345c3926ffb157418
Magesh Kuppan <tkmagesh77@gmail.com>
2022-09-21 11:46:10 +0530

    intro to structs

---
+++ 10-structs/intro.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+type Employee struct {
+	Id   int
+	Name string
+	City string
+}
+
+func main() {
+	//var emp Employee
+	//var emp Employee = Employee{100, "Magesh", "Bengaluru"}
+	//var emp Employee = Employee{Id: 100, Name: "Magesh"}
+	/*
+		var emp Employee = Employee{
+			Id:   100,
+			Name: "Magesh",
+			City: "Bengaluru",
+		}
+	*/
+	/*
+		var emp = Employee{
+			Id:   100,
+			Name: "Magesh",
+			City: "Bengaluru",
+		}
+	*/
+
+	emp := Employee{
+		Id:   100,
+		Name: "Magesh",
+		City: "Bengaluru",
+	}
+
+	//pointer
+	//empPtr := new(Employee)
+
+	empPtr := &Employee{
+		Id:   100,
+		Name: "Magesh",
+		City: "Bengaluru",
+	}
+
+	//fmt.Println(emp)
+	fmt.Printf("%#v\n", emp)
+
+	fmt.Println("Accessing the fields")
+	fmt.Printf("Name = %q\n", emp.Name)
+
+	fmt.Println("Accessing the fileds (pointer)")
+	//fmt.Printf("Name = %q\n", (*empPtr).Name)
+	fmt.Printf("Name = %q\n", empPtr.Name)
+
+	emp2 := Employee{
+		Id:   100,
+		Name: "Magesh",
+		City: "Bengaluru",
+	}
+	fmt.Println(emp == emp2)
+}
