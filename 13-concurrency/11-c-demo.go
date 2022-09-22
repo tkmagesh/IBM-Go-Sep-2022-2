f64dc7058cebf8e6e0788cc1bc2c5a0dcf369b23
Magesh Kuppan <tkmagesh77@gmail.com>
2022-09-22 13:00:34 +0530

    more on channels

--- 13-concurrency/11-c-demo.go
+++ 13-concurrency/11-c-demo.go
@@ -7,18 +7,25 @@ import (
 
 //Share memory by communicating
 
-func main() {
+func main() { //consumer
 	fmt.Println("main started")
 	ch := add(100, 200)
+	/*
+		go func() {
+			ch <- 1000
+		}()
+	*/
 	result := <-ch //receive operation
 	fmt.Println("result =", result)
 	fmt.Println("main completed")
 }
 
-func add(x, y int) chan int {
+func add(x, y int) <-chan int { //producer
 	ch := make(chan int)
-	time.Sleep(3 * time.Second)
-	result := x + y
-	ch <- result //send operation
+	go func() {
+		time.Sleep(3 * time.Second)
+		result := x + y
+		ch <- result //send operation
+	}()
 	return ch
 }
