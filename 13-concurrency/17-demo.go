77810d76140831d9e3602cd7d0664590e66848b0
Magesh Kuppan <tkmagesh77@gmail.com>
2022-09-22 15:04:10 +0530

    more on select case

---
+++ 13-concurrency/17-demo.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	ch := generateNos()
+	for data := range ch {
+		fmt.Println(data)
+	}
+}
+
+func generateNos() <-chan int {
+	ch := make(chan int)
+	timeoutCh := timeout(10 * time.Second)
+	go func() {
+		var no int = 1
+	LOOP:
+		for {
+			select {
+			case ch <- no * 10:
+				time.Sleep(500 * time.Millisecond)
+				no++
+			case <-timeoutCh:
+				fmt.Println("Timeout tiggered")
+				break LOOP
+			}
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+func timeout(d time.Duration) <-chan time.Time {
+	timeoutCh := make(chan time.Time)
+	go func() {
+		time.Sleep(d)
+		timeoutCh <- time.Now()
+	}()
+	return timeoutCh
+}
