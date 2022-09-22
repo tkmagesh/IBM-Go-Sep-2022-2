be45b4da64e9337c34be6cc6b46f0e873ea25ff3
Magesh Kuppan <tkmagesh77@gmail.com>
2022-09-22 15:12:47 +0530

    assignment

---
+++ 13-concurrency/19-demo.go
@@ -0,0 +1,50 @@
+package main
+
+/* Modify the program in such a way that the program keeps generating the numbers until the user hits the ENTER key */
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	ch := generateNos()
+	for data := range ch {
+		fmt.Println(data)
+	}
+}
+
+func generateNos() <-chan int {
+	ch := make(chan int)
+	timeoutCh := time.After(10 * time.Second)
+	go func() {
+		var no int = 1
+	LOOP:
+		for {
+			select {
+			case ch <- no * 10:
+				time.Sleep(500 * time.Millisecond)
+				no++
+			case <-timeoutCh:
+				fmt.Println("Timeout tiggered")
+				break LOOP
+			}
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+/*
+//using time.After instead
+
+func timeout(d time.Duration) <-chan time.Time {
+	timeoutCh := make(chan time.Time)
+	go func() {
+		time.Sleep(d)
+		timeoutCh <- time.Now()
+	}()
+	return timeoutCh
+}
+*/
